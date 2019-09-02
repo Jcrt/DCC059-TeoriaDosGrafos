72373ef65437ab1d34d98489154b474555edcc4c
jcrt <julio.rosatrindade@gmail.com>
2019-09-02 00:12:53 -0300

    Tentativa de consertar a branch

--- main.cpp
+++ main.cpp
@@ -11,21 +11,21 @@ int main() {
         MenuPrincipal::ReadOption();
     }*/
 
-    Grafo g;
+    Grafo *g = new Grafo(10, false, false, false);
     for(int i = 0; i <= 10; i++){
-        g.insereNo(i);
+        g->insereNo(i);
     }
     cout << endl;
 
-    g.addAresta(1, 10, 20);
-    g.addAresta(1, 7, 10);
-    g.addAresta(3, 5, 13);
-    g.addAresta(5, 6, 19);
-    g.addAresta(6, 9, 30);
+    g->addAresta(1, 10, 20);
+    g->addAresta(1, 7, 10);
+    g->addAresta(3, 5, 13);
+    g->addAresta(5, 6, 19);
+    g->addAresta(6, 9, 30);
     cout << endl;
 
-    g.removeNo(1);
-    g.removeNo((20));
-    g.removeAresta(6, 5);
-    g.removeAresta(4, 8);
+    g->removeNo(1);
+    g->removeNo((20));
+    g->removeAresta(6, 5);
+    g->removeAresta(4, 8);
 }
